7d0d000f40776608db48a1809c828b7367ee6543
Colugo <colugomusic@gmail.com>
2026-06-27 14:39:57 +0100

    Use a per-slot node pool to reuse nodes (14% faster)

--- include/atom.hpp
+++ include/atom.hpp
@@ -33,13 +33,31 @@ struct retire_list {
 	node<T>* head = nullptr;
 };
 
-template <typename T> using node_list = std::vector<const node<T>*>;
+template <typename T> using const_node_list = std::vector<const node<T>*>;
+template <typename T> using node_list       = std::vector<node<T>*>;
 
 template <typename T>
-auto init_protected_node_list_for_gc() -> node_list<T> {
-	auto list = node_list<T>{};
-	list.reserve(std::max(1u, std::thread::hardware_concurrency()) * 2);
+auto init_protected_node_list_for_gc(size_t count = std::max(1u, std::thread::hardware_concurrency()) * 2) -> const_node_list<T> {
+	auto list = const_node_list<T>{};
+	list.reserve(count);
 	return list;
+}
+
+template <typename T>
+auto init_node_pool(size_t count = std::max(1u, std::thread::hardware_concurrency())) -> node_list<T> {
+	auto list = node_list<T>{};
+	list.reserve(count);
+	for (size_t i = 0; i < count; ++i) {
+		list.push_back(new node<T>{});
+	}
+	return list;
+}
+
+template <typename T>
+auto delete_all(const node_list<T>& nodes) -> void {
+	for (auto node : nodes) {
+		delete node;
+	}
 }
 
 template <typename T>
@@ -47,16 +65,18 @@ struct slot {
 	slot() = default;
 	~slot();
 	hazptr::retire_list<T> retire_list;
-	std::atomic<slot*> next                 = nullptr;
-	std::atomic<bool> assigned              = true;
-	std::atomic<node<T>*> protected_node    = nullptr;
-	int num_retires_since_gc                = 0;
-	node_list<T> protected_node_list_for_gc = init_protected_node_list_for_gc<T>();
+	std::atomic<slot*> next                       = nullptr;
+	std::atomic<bool> in_use                      = false; // Assigned to a thread?
+	std::atomic<node<T>*> protected_node          = nullptr;
+	int num_retires_since_gc                      = 0;
+	const_node_list<T> protected_node_list_for_gc = init_protected_node_list_for_gc<T>();
+	node_list<T> node_pool                        = init_node_pool<T>();
 };
 
 template <typename T>
 struct hazptrs {
 	hazptrs();
+	~hazptrs();
 	slot<T>* slot_list = nullptr;
 };
 
@@ -70,14 +90,14 @@ private:
 };
 
 template <typename T>
-auto g_hazptrs() -> hazptrs<T>* {
-	static hazptrs<T> g;
-	return &g;
+auto g_hazptrs() -> std::shared_ptr<hazptrs<T>> {
+	static auto g = std::make_shared<hazptrs<T>>();
+	return g;
 }
 
 template <typename T>
 auto tls_slot() -> slot<T>* {
-	thread_local thread_slot<T> g{g_hazptrs<T>()};
+	thread_local thread_slot<T> g{g_hazptrs<T>().get()};
 	return g.get();
 }
 
@@ -87,45 +107,104 @@ auto push(hazptr::retire_list<T>* retire_list, hazptr::node<T>* node) -> void {
 }
 
 template <typename T> [[nodiscard]]
-auto init_slot_list(std::size_t count) -> slot<T>* {
-	slot<T>* head = nullptr;
+auto init_list(std::size_t count) -> T* {
+	T* head = nullptr;
 	for (std::size_t i = 0; i < count; ++i) {
-		auto new_slot = new slot<T>{};
-		new_slot->next = head;
-		head = new_slot;
+		auto new_item = new T{};
+		new_item->next = head;
+		head = new_item;
 	}
 	return head;
 }
 
-template <typename T> [[nodiscard]]
-auto create_new_thread_slot_at_tail(hazptr::slot<T>* tail) -> slot<T>* {
-	auto new_slot = new slot<T>{};
-	slot<T>* next = nullptr;
-	while (!tail->next.compare_exchange_weak(next, new_slot)) {
-		tail = next;
-		next = nullptr;
+template <typename T>
+auto delete_list(T* head) -> void {
+	while (head) {
+		auto old = std::exchange(head, head->next);
+		delete old;
 	}
-	return new_slot;
 }
 
 template <typename T> [[nodiscard]]
-auto acquire_thread_slot(hazptr::hazptrs<T>* hazptrs) -> slot<T>* {
-	assert (hazptrs);
-	auto curr = hazptrs->slot_list;
+auto init_slot_list(std::size_t count = std::max(1u, std::thread::hardware_concurrency())) -> slot<T>* {
+	return init_list<slot<T>>(count);
+}
+
+template <typename T> [[nodiscard]]
+auto create_new_item_at_tail(T* tail) -> T* {
+	auto new_item = new T{};
+	new_item->in_use.store(true);
+	T* next = nullptr;
+	while (!tail->next.compare_exchange_weak(next, new_item)) {
+		tail = next;
+		next = nullptr;
+	}
+	return new_item;
+}
+
+template <typename T> [[nodiscard]]
+auto acquire_item_from_list(T* list) -> T* {
+	auto curr = list;
 	for (;;) {
-		if (!curr->assigned.load() && !curr->assigned.exchange(true)) {
+		if (!curr->in_use.load() && !curr->in_use.exchange(true)) {
 			return curr;
 		}
 		if (!curr->next.load()) {
-			return create_new_thread_slot_at_tail(curr);
+			return create_new_item_at_tail(curr);
 		}
 		curr = curr->next.load();
 	}
 }
 
+template <typename T> [[nodiscard]]
+auto acquire_thread_slot(hazptr::hazptrs<T>* hazptrs) -> slot<T>* {
+	assert (hazptrs);
+	return acquire_item_from_list(hazptrs->slot_list);
+}
+
+template <typename T> [[nodiscard]]
+auto acquire_node(node_list<T>* pool, T value) -> node<T>* {
+	assert (pool);
+	if (pool->empty()) {
+		return new node<T>{std::move(value)};
+	}
+	auto node = pool->back();
+	pool->pop_back();
+	node->value = std::move(value);
+	return node;
+}
+
+template <typename T> [[nodiscard]]
+auto acquire_node(hazptr::slot<T>* slot, T value) -> node<T>* {
+	assert (slot);
+	return acquire_node(&slot->node_pool, std::move(value));
+}
+
+template <typename T> [[nodiscard]]
+auto acquire_node(T value) -> node<T>* {
+	return acquire_node(tls_slot<T>(), std::move(value));
+}
+
+template <typename T>
+auto release_node(node_list<T>* pool, hazptr::node<T>* node) -> void {
+	assert (pool);
+	pool->push_back(node);
+}
+
+template <typename T>
+auto release_node(hazptr::slot<T>* slot, hazptr::node<T>* node) -> void {
+	assert (slot);
+	release_node(&slot->node_pool, node);
+}
+
+template <typename T>
+auto release_node(hazptr::node<T>* node) -> void {
+	release_node(tls_slot<T>(), node);
+}
+
 template <typename T>
 auto release_thread_slot(hazptr::slot<T>* slot) -> void {
-	slot->assigned.store(false);
+	slot->in_use.store(false);
 }
 
 template <typename T, typename Fn>
@@ -155,7 +234,7 @@ auto sort_and_remove_duplicates(std::vector<T, Alloc>* x) -> void {
 }
 
 template <typename T>
-auto find_protected_nodes(hazptr::hazptrs<T>* hazptrs, node_list<T>* list) -> void {
+auto find_protected_nodes(hazptr::hazptrs<T>* hazptrs, const_node_list<T>* list) -> void {
 	assert (hazptrs);
 	assert (list->empty());
 	auto add_to_list = [list](const hazptr::node<T>* node) {
@@ -166,10 +245,10 @@ auto find_protected_nodes(hazptr::hazptrs<T>* hazptrs, node_list<T>* list) -> vo
 }
 
 template <typename T, typename FnIsProtected>
-auto delete_unprotected_nodes(hazptr::retire_list<T>* retire_list, FnIsProtected is_protected) -> void {
+auto release_unprotected_nodes(hazptr::retire_list<T>* retire_list, node_list<T>* pool, FnIsProtected is_protected) -> void {
 	while (retire_list->head && !is_protected(retire_list->head)) {
 		auto old = std::exchange(retire_list->head, retire_list->head->next);
-		delete old;
+		release_node(pool, old);
 	}
 	if (retire_list->head) {
 		auto prev = retire_list->head;
@@ -177,7 +256,7 @@ auto delete_unprotected_nodes(hazptr::retire_list<T>* retire_list, FnIsProtected
 		while (curr) {
 			if (!is_protected(curr)) {
 				auto old = std::exchange(curr, curr->next);
-				delete old;
+				release_node(pool, old);
 				prev->next = curr;
 			}
 			else {
@@ -195,7 +274,7 @@ auto gc(hazptr::hazptrs<T>* hazptrs, hazptr::slot<T>* slot) -> void {
 	auto is_protected = [slot](const hazptr::node<T>* node) {
 		return std::ranges::binary_search(slot->protected_node_list_for_gc, node);
 	};
-	delete_unprotected_nodes(&slot->retire_list, is_protected);
+	release_unprotected_nodes(&slot->retire_list, &slot->node_pool, is_protected);
 	slot->protected_node_list_for_gc.clear();
 }
 
@@ -231,7 +310,7 @@ auto release(hazptr::slot<T>* slot) -> void {
 
 template <typename T>
 auto retire(hazptr::node<T>* node, int gc_threshold) -> void {
-	retire(g_hazptrs<T>(), tls_slot<T>(), node, gc_threshold);
+	retire(g_hazptrs<T>().get(), tls_slot<T>(), node, gc_threshold);
 }
 
 template <typename T>
@@ -246,10 +325,11 @@ auto release() -> void {
 
 [[nodiscard]] auto fn_always(auto value) { return [value](auto&&...) { return value; }; }
 
-template <typename T> hazptrs<T>::hazptrs() : slot_list{init_slot_list<T>(std::max(1u, std::thread::hardware_concurrency()))} {}
+template <typename T> hazptrs<T>::hazptrs() : slot_list{init_slot_list<T>()} {}
+template <typename T> hazptrs<T>::~hazptrs() { delete_list(slot_list); }
 template <typename T> thread_slot<T>::thread_slot(hazptr::hazptrs<T>* hazptrs) : slot_{acquire_thread_slot(hazptrs)} {}
 template <typename T> thread_slot<T>::~thread_slot() { release_thread_slot(slot_); }
-template <typename T> slot<T>::~slot() { delete_unprotected_nodes(&retire_list, fn_always(false)); }
+template <typename T> slot<T>::~slot() { release_unprotected_nodes(&retire_list, &node_pool, fn_always(false)); delete_all(node_pool); }
 
 } // atom::hazptr
 
@@ -263,13 +343,18 @@ struct val {
 	auto apply(auto fn) -> T;
 	auto apply_r(auto fn) -> decltype(auto);
 private:
+	// We only hold this shared_ptr to keep hazptrs<T> alive
+	// which allows clients to create val<T>s with static
+	// storage duration without leaking memory at shutdown.
+	std::shared_ptr<hazptr::hazptrs<T>> hazptrs_;
 	mutable std::atomic<hazptr::node<T>*> node_;
 	int gc_threshold_;
 };
 
 template <typename T>
 val<T>::val(int gc_threshold)
-	: node_{new hazptr::node<T>{}}
+	: hazptrs_{hazptr::g_hazptrs<T>()}
+	, node_{hazptr::acquire_node<T>(T{})}
 	, gc_threshold_{gc_threshold}
 {
 }
@@ -286,12 +371,12 @@ auto val<T>::apply(auto fn) -> T {
 		auto old_value     = expected_node->value;
 		hazptr::release<T>();
 		const auto new_value    = fn(std::move(old_value));
-		const auto desired_node = new hazptr::node<T>{.value = new_value};
+		const auto desired_node = hazptr::acquire_node<T>(new_value);
 		if (node_.compare_exchange_weak(expected_node, desired_node)) {
 			hazptr::retire(expected_node, gc_threshold_);
 			return new_value;
 		}
-		delete desired_node;
+		hazptr::release_node(desired_node);
 	}
 }
 
@@ -302,12 +387,12 @@ auto val<T>::apply_r(auto fn) -> decltype(auto) {
 		auto old_value     = expected_node->value;
 		hazptr::release<T>();
 		const auto [new_value, result] = fn(std::move(old_value));
-		const auto desired_node        = new hazptr::node<T>{.value = new_value};
+		const auto desired_node        = hazptr::acquire_node<T>(new_value);
 		if (node_.compare_exchange_weak(expected_node, desired_node)) {
 			hazptr::retire(expected_node, gc_threshold_);
 			return result;
 		}
-		delete desired_node;
+		hazptr::release_node(desired_node);
 	}
 }
 
--- test/src/main.cpp
+++ test/src/main.cpp
@@ -16,6 +16,16 @@ struct model_b {
 	char c = 'A';
 };
 
+struct profiling {
+	int print_count       = 0;
+	int counter_dec_count = 0;
+	int counter_inc_count = 0;
+	int counter_set_count = 0;
+	int counter_ret_count = 0;
+	int char_inc_count    = 0;
+	int char_dec_count    = 0;
+};
+
 auto prev(char c) -> char {
 	if (--c < '!') { c = '~'; }
 	return c;
@@ -42,73 +52,102 @@ auto fn_set_counter(int v) { return [v](model_a x) { x.counter = v; return x; };
 auto print_counter(const model_a& v) -> void { std::cout << std::format("counter: {}\n", v.counter); }
 auto print_char(const model_b& v) -> void    { std::cout << std::format("character: {}\n", v.c); }
 
-auto printer_thread(std::stop_token stop, atom::val<model_a>* a0, atom::val<model_a>* a1, atom::val<model_b>* b0, atom::val<model_b>* b1) -> void {
+auto printer_thread(std::stop_token stop, atom::val<model_a>* a0, atom::val<model_a>* a1, atom::val<model_b>* b0, atom::val<model_b>* b1, profiling* prof) -> void {
 	while (!stop.stop_requested()) {
 		print_counter(a0->get());
 		print_counter(a1->get());
 		print_char(b0->get());
 		print_char(b1->get());
+		prof->print_count++;
 	}
 }
 
-auto counter_decrementer_thread(std::stop_token stop, atom::val<model_a>* a) -> void {
+auto counter_decrementer_thread(std::stop_token stop, atom::val<model_a>* a, profiling* prof) -> void {
 	while (!stop.stop_requested()) {
 		a->apply(fn_decrement_counter);
+		prof->counter_dec_count++;
 	}
 }
 
-auto counter_incrementer_thread(std::stop_token stop, atom::val<model_a>* a) -> void {
+auto counter_incrementer_thread(std::stop_token stop, atom::val<model_a>* a, profiling* prof) -> void {
 	while (!stop.stop_requested()) {
 		a->apply(fn_increment_counter);
+		prof->counter_inc_count++;
 	}
 }
 
-auto counter_setter_thread(std::stop_token stop, atom::val<model_a>* a) -> void {
+auto counter_setter_thread(std::stop_token stop, atom::val<model_a>* a, profiling* prof) -> void {
 	while (!stop.stop_requested()) {
 		a->apply(fn_set_counter(0));
+		prof->counter_set_count++;
 	}
 }
 
-auto return_42_thread(std::stop_token stop, atom::val<model_a>* a) -> void {
+auto return_42_thread(std::stop_token stop, atom::val<model_a>* a, profiling* prof) -> void {
 	while (!stop.stop_requested()) {
 		const auto value = a->apply_r(fn_just_return_42);
 		assert (value == 42);
+		prof->counter_ret_count++;
 	}
 }
 
-auto char_decrementer_thread(std::stop_token stop, atom::val<model_b>* b) -> void {
+auto char_decrementer_thread(std::stop_token stop, atom::val<model_b>* b, profiling* prof) -> void {
 	while (!stop.stop_requested()) {
 		b->apply(fn_prev_char);
+		prof->char_dec_count++;
 	}
 }
 
-auto char_incrementer_thread(std::stop_token stop, atom::val<model_b>* b) -> void {
+auto char_incrementer_thread(std::stop_token stop, atom::val<model_b>* b, profiling* prof) -> void {
 	while (!stop.stop_requested()) {
 		b->apply(fn_next_char);
+		prof->char_inc_count++;
 	}
 }
 
 auto main(int, const char*[]) -> int {
-	// We create multiple vals of of the same <T> as they will be sharing
-	// hazard pointer slots under the hood so we just want to check
-	// everything is ok with that sharing.
-	auto a0             = atom::val<model_a>{};
-	auto a1             = atom::val<model_a>{};
-	auto b0             = atom::val<model_b>{};
-	auto b1             = atom::val<model_b>{};
-	auto a0_counter_dec = std::jthread{counter_decrementer_thread, &a0};
-	auto a0_counter_inc = std::jthread{counter_incrementer_thread, &a0};
-	auto a0_counter_set = std::jthread{counter_setter_thread, &a0};
-	auto a0_return_42   = std::jthread{return_42_thread, &a0};
-	auto b0_char_dec    = std::jthread{char_decrementer_thread, &b0};
-	auto b0_char_inc    = std::jthread{char_incrementer_thread, &b0};
-	auto a1_counter_dec = std::jthread{counter_decrementer_thread, &a1};
-	auto a1_counter_inc = std::jthread{counter_incrementer_thread, &a1};
-	auto a1_counter_set = std::jthread{counter_setter_thread, &a1};
-	auto a1_return_42   = std::jthread{return_42_thread, &a1};
-	auto b1_char_dec    = std::jthread{char_decrementer_thread, &b1};
-	auto b1_char_inc    = std::jthread{char_incrementer_thread, &b1};
-	auto printer        = std::jthread{printer_thread, &a0, &a1, &b0, &b1};
-	std::this_thread::sleep_for(RUN_DURATION);
+	auto prof = profiling{};
+	{
+		// We create multiple vals of of the same <T> as they will be sharing
+		// hazard pointer slots under the hood so we just want to check
+		// everything is ok with that sharing.
+		auto a0             = atom::val<model_a>{};
+		auto a1             = atom::val<model_a>{};
+		auto b0             = atom::val<model_b>{};
+		auto b1             = atom::val<model_b>{};
+		auto a0_counter_dec = std::jthread{counter_decrementer_thread, &a0, &prof};
+		auto a0_counter_inc = std::jthread{counter_incrementer_thread, &a0, &prof};
+		auto a0_counter_set = std::jthread{counter_setter_thread, &a0, &prof};
+		auto a0_return_42   = std::jthread{return_42_thread, &a0, &prof};
+		auto b0_char_dec    = std::jthread{char_decrementer_thread, &b0, &prof};
+		auto b0_char_inc    = std::jthread{char_incrementer_thread, &b0, &prof};
+		auto a1_counter_dec = std::jthread{counter_decrementer_thread, &a1, &prof};
+		auto a1_counter_inc = std::jthread{counter_incrementer_thread, &a1, &prof};
+		auto a1_counter_set = std::jthread{counter_setter_thread, &a1, &prof};
+		auto a1_return_42   = std::jthread{return_42_thread, &a1, &prof};
+		auto b1_char_dec    = std::jthread{char_decrementer_thread, &b1, &prof};
+		auto b1_char_inc    = std::jthread{char_incrementer_thread, &b1, &prof};
+		auto printer        = std::jthread{printer_thread, &a0, &a1, &b0, &b1, &prof};
+		std::this_thread::sleep_for(RUN_DURATION);
+	}
+	std::cout << std::format(
+		"\nProfiling results:\n"
+		"print: {}\n"
+		"counter dec: {}\n"
+		"counter inc: {}\n"
+		"counter set: {}\n"
+		"counter ret: {}\n"
+		"char dec: {}\n"
+		"char inc: {}\n"
+		"---------------------\n"
+		"total: {}\n",
+		prof.print_count,
+		prof.counter_dec_count,
+		prof.counter_inc_count,
+		prof.counter_set_count,
+		prof.counter_ret_count,
+		prof.char_dec_count,
+		prof.char_inc_count,
+		prof.print_count + prof.counter_dec_count + prof.counter_inc_count + prof.counter_set_count + prof.counter_ret_count + prof.char_dec_count + prof.char_inc_count);
 	return 0;
 }
